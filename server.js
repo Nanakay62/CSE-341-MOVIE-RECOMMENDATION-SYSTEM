2aef746799ac11807c16eb5e40f4c747990db22a
Nana Kwame Osei Ofori Dickson <dic20016@byui.edu>
2023-06-20 10:00:21 +0000

    Added the controller, inputs in the server and created swagger.js

--- server.js
+++ server.js
@@ -1,4 +1,46 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { initDb } = require('./db/connect');
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const setupSwagger = require('./swagger');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+const port = 8000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use('/', routes);
+
+// MongoDB connection
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Initialize the database connection
+initDb((err) => {
+  if (err) {
+    console.error('Sorry, there was an error connecting to the database', err);
+    process.exit(1);
+  } else {
+    // Setup Swagger API documentation
+    setupSwagger(app);
+
+    app.listen(process.env.PORT || port, () => {
+      console.log(`Web Server is listening at port ${port}`);
+      console.log('Database connected');
+    });
+  }});
+
+
+
+/*const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
@@ -30,4 +72,4 @@ const swaggerOptions = {
 
 app.listen(process.env.PORT || port,() => {
 console.log(`Server listening at ${port}`)
-})
+})*/
---
+++ swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Movie Recommendation API',
+      version: '1.0.0',
+      description: 'API endpoints for managing movie recommendations',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // Replace with your server URL
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to your route files
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
